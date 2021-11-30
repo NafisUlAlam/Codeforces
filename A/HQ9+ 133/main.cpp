e7a892c7c8e1c192e4d18e8aa1b1f334bc8d1c01
Nafis Ul Alam <ahmednafis74@gmail.com>
2021-11-30 22:04:11 +0600

    hq9+ edited

--- A/HQ9+ 133/main.cpp
+++ A/HQ9+ 133/main.cpp
@@ -8,7 +8,7 @@ int main()
     string s;
     cin >> s;
     for (int i = 0; i < s.size(); i++){
-        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9' || s[i] == '+'){
+        if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9'){
             cout << "YES";
             flag = true;
             break;
